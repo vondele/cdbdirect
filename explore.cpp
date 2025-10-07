9078f6de610620e7d80d7a8b325ecb2eac7edfd7
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-10-07 22:30:40 +0200

    More interesting prototype

--- explore.cpp
+++ explore.cpp
@@ -1,4 +1,5 @@
 #include <atomic>
+#include <chrono>
 #include <cstdint>
 #include <iostream>
 
@@ -6,78 +7,30 @@
 #include "rocksdb/filter_policy.h"
 #include "rocksdb/options.h"
 #include "rocksdb/table.h"
-#include "table/terark_zip_table.h"
+
+#include "cdbdirect.h"
 
 using namespace TERARKDB_NAMESPACE;
 
-// Setup stuff
+void IterateRange(
+    DB *db, const RangeStorage &range,
+    const std::function<bool(const std::string &,
+                             const std::vector<std::pair<std::string, int>> &)>
+        &evaluate_entry) {
 
-// Initialize the DB given a path, and return a handle for later use
-std::uintptr_t cdbdirect_initialize(const std::string &path) {
-
-  TerarkZipTableOptions tzt_options;
-  // TerarkZipTable requires a temp directory other than data directory, a slow
-  // device is acceptable
-  tzt_options.localTempDir = "/tmp";
-
-  BlockBasedTableOptions table_options;
-  table_options.block_cache = NewLRUCache(32 * 1024 * 1024 * 1024LL);
-  // table_options.no_block_cache = true;
-  Options options;
-  options.IncreaseParallelism();
-  options.table_factory.reset(
-      NewTerarkZipTableFactory(tzt_options, options.table_factory));
-
-  DB *db;
-
-  // open DB
-  Status s = DB::OpenForReadOnly(options, path, &db);
-  if (!s.ok())
-    std::cerr << s.ToString() << std::endl;
-  assert(s.ok());
-
-  return reinterpret_cast<std::uintptr_t>(db);
-}
-
-// Return the size of the DB
-std::uint64_t cdbdirect_size(std::uintptr_t handle) {
-
-  DB *db = reinterpret_cast<DB *>(handle);
-
-  std::uint64_t size[1];
-  db->GetIntProperty("rocksdb.estimate-num-keys", size);
-
-  return size[0];
-}
-
-// Finalize the DB
-std::uintptr_t cdbdirect_finalize(std::uintptr_t handle) {
-
-  DB *db = reinterpret_cast<DB *>(handle);
-
-  // safely close the DB.
-  delete db;
-
-  return 0;
-}
-
-std::atomic<size_t> count(0);
-
-void IterateRange(DB *db, const RangeStorage &range) {
-
-  std::unique_ptr<Iterator> it(db->NewIterator(ReadOptions()));
   const Comparator *cmp = db->GetOptions().comparator;
+  std::unique_ptr<Iterator> it(db->NewIterator(ReadOptions()));
 
   for (it->Seek(range.start);
        it->Valid() && (cmp->Compare(it->key(), range.limit) < 0); it->Next()) {
 
-    count.fetch_add(1, std::memory_order_relaxed);
+    auto fen = key_to_fen(it->key().ToString(), true);
+    auto scored = value_to_scoredMoves(it->value().ToString(), true);
+    if (!evaluate_entry(fen, scored))
+      break;
   }
 }
 
-//
-// just split the full range in non-overlapping ranges based on SST files
-//
 std::vector<RangeStorage> BuildRangesFromSSTs(DB *db, size_t num_threads) {
 
   std::unique_ptr<Iterator> it(db->NewIterator(ReadOptions()));
@@ -100,13 +53,12 @@ std::vector<RangeStorage> BuildRangesFromSSTs(DB *db, size_t num_threads) {
     merged.push_back(RangeStorage(
         files[i].smallestkey,
         (i + 1 == files.size())
-            ? last_key_str + "z" // add something, so the last key is included
+            ? last_key_str + "z" // Adding 'z' to ensure inclusion of last key
             : files[i + 1].smallestkey));
   }
 
-  std::cout << "Compare: " << cmp->Compare(last_key_str, merged.back().limit)
-            << std::endl;
-
+  // Now partition merged ranges evenly by count or total file size
+  // Partition merged ranges as evenly as possible among threads
   size_t num_ranges = std::min(num_threads, merged.size());
   std::vector<RangeStorage> out;
   size_t per_range = merged.size() / num_ranges;
@@ -114,33 +66,80 @@ std::vector<RangeStorage> BuildRangesFromSSTs(DB *db, size_t num_threads) {
   size_t idx = 0;
   for (size_t i = 0; i < num_ranges; ++i) {
     size_t chunk = per_range + (i < remainder ? 1 : 0);
-    RangeStorage r =
-        RangeStorage(merged[idx].start, merged[idx + chunk - 1].limit);
-    out.push_back(r);
+    out.push_back(
+        RangeStorage(merged[idx].start, merged[idx + chunk - 1].limit));
     idx += chunk;
   }
+
   return out;
 }
 
 int main() {
   std::uintptr_t handle = cdbdirect_initialize(CHESSDB_PATH);
 
-  std::uint64_t size = cdbdirect_size(handle);
-  std::cout << "DB count: " << size << std::endl;
+  std::uint64_t db_size = cdbdirect_size(handle);
+  std::cout << "DB count: " << db_size << std::endl;
 
   DB *db = reinterpret_cast<DB *>(handle);
 
-  const size_t num_threads = 32;
+  const size_t num_threads = std::thread::hardware_concurrency();
   auto ranges = BuildRangesFromSSTs(db, num_threads);
+  std::atomic<size_t> count_total(0);
+  std::atomic<size_t> count_have_minply(0);
+  std::atomic<size_t> count_have_single(0);
+  std::atomic<size_t> count_moves(0);
+  auto start = std::chrono::steady_clock::now();
+
+  auto evaluate_entry =
+      [&](const std::string &fen,
+          const std::vector<std::pair<std::string, int>> &scored) {
+        // count entries
+        size_t peek = count_total.fetch_add(1, std::memory_order_relaxed);
+        if (scored.back().second > -1)
+          count_have_minply.fetch_add(1, std::memory_order_relaxed);
+        if (scored.size() == 2)
+          count_have_single.fetch_add(1, std::memory_order_relaxed);
+        count_moves.fetch_add(scored.size() - 1, std::memory_order_relaxed);
+
+        // status update
+        if (peek % 10000000 == 0 && peek > 0) {
+          auto end = std::chrono::steady_clock::now();
+          auto elapsed =
+              std::chrono::duration_cast<std::chrono::seconds>(end - start);
+          std::cout << "Counted               " << peek << " entries so far..."
+                    << "\n";
+          std::cout << "  Have min ply:       " << count_have_minply.load()
+                    << "\n";
+          std::cout << "  Have single move:   " << count_have_single.load()
+                    << "\n";
+          std::cout << "  Total scored moves: " << count_moves.load() << "\n";
+          std::cout << "  Time (s):           " << elapsed.count() << "\n";
+          std::cout << "  nps:                " << peek / elapsed.count()
+                    << "\n";
+          std::cout << "  ETA (s):            "
+                    << (db_size - peek) * elapsed.count() / peek << "\n";
+          /*
+          std::cout << fen << "\n";
+          for (auto &e : scored) {
+            std::cout << e.first << " " << e.second << "\n";
+          }
+          */
+          std::cout << std::endl;
+        }
+        return true; // continue iteration
+      };
 
   std::vector<std::thread> workers;
   for (auto &r : ranges) {
-    workers.emplace_back(IterateRange, db, r);
+    workers.emplace_back(IterateRange, db, r, evaluate_entry);
   }
   for (auto &t : workers)
     t.join();
 
-  std::cout << "Exact count: " << count << std::endl;
+  std::cout << "Exact count:          " << count_total << std::endl;
+  std::cout << "  Have min ply:       " << count_have_minply.load() << "\n";
+  std::cout << "  Have single move:   " << count_have_single.load() << "\n";
+  std::cout << "  Total scored moves: " << count_moves.load() << "\n";
 
   cdbdirect_finalize(handle);
   return 0;
